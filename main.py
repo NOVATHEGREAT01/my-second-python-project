5852008ae5af890211563b7f11a9622a408e945f
NOVA THE GREAT <jeffersonidehen00@gmail.com>
2026-06-22 20:47:05 +0100

    finised data()

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 class Courses():
-    def __init__(self,courses,name,score):
+    def __init__(self,courses,name,score,grade):
         self.courses = courses
         self.name = name
         self.score = score
+        self.grade = grade
 
     def __str__(self):
         return f'Hello {self.name}'
@@ -12,26 +13,41 @@ def data():
         try:
             course.name = str(input('name: '))
             if course.name == '':
-                d
+                data()
+            
             course.score = int(input('score: '))
             if course.score == '':
-                d
+                data()
 
         except ValueError:
-            print('Invalid datatype \n name = "STRING" and score = "INTEGER"')
+            data()
 
         else:
-            print(course.name)
-            print(course.score)
+            if course.score >= 70:
+                course.grade = 'A'
+                print(f'{course.name} your grade is {course.grade}')
+
+            elif course.score < 70 and course.score >= 60:
+                course.grade = 'B'
+                print(f'{course.name} your grade is {course.grade}')
+
+            elif course.score < 60 and course.score >= 50:
+                course.grade = 'c'
+                print(f'{course.name} your grade is {course.grade}')
+
+            elif course.score < 50 and course.score >= 40:
+                course.grade = 'D'
+                print(f'{course.name} your grade is {course.grade}')
+
+            elif course.score < 40:
+                course.grade = 'F'
+                print(f'{course.name} your grade is {course.grade}')
+
         break
-        
 
+course = Courses(['MTH','PHY','CHM','STA','COS'],name = '',score = '',grade = '')
 
-
-course = Courses(['MTH','PHY','CHM','STA','COS'],name = '',score = '')
-
-
-d = data()
+data()
 
 
 
