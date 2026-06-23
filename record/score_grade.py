0050b7f3729b7e4f6c497bc1468ea0af67592e9f
NOVA THE GREAT <jeffersonidehen00@gmail.com>
2026-06-23 12:48:25 +0100

    editted score_grade

--- record/score_grade.py
+++ record/score_grade.py
@@ -14,26 +14,26 @@ def score_grade():
         else:
             if student.score >= 70:
                 student.grade = 'A'
-                print(f'{student.name} your grade is {student.grade}')
+                print(student.grade)
 
             elif student.score < 70 and student.score >= 60:
                 student.grade = 'B'
-                print(f'{student.name} your grade is {student.grade}')
+                print(student.grade)
 
             elif student.score < 60 and student.score >= 50:
                 student.grade = 'c'
-                print(f'{student.name} your grade is {student.grade}')
+                print(student.grade)
 
             elif student.score < 50 and student.score >= 40:
                 student.grade = 'D'
-                print(f'{student.name} your grade is {student.grade}')
+                print(student.grade)
 
             elif student.score < 40 and student.score >= 30:
                 student.grade = 'E'
-                print(f'{student.name} your grade is {student.grade}')
+                print(student.grade)
 
             elif student.score < 30:
                 student.grade = 'F'
-                print(f'{student.name} your grade is {student.grade}')
+                print(student.grade)
 
         break
